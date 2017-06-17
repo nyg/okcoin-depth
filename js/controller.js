d0db7ede980577f48d99f83fc5d81a10a71bd824
nyg <nyg>
2017-06-17 17:08:30 +0200

    Updated okcoin-api to latest version.

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,11 @@
 view.restoreApiKeys() // from localStorage
 
-var api = new OKCoin('com', {
+new OKCoin()
+.setChannels({
     ok_sub_futureusd_btc_index: handleIndex,
     ok_sub_future_btc_depth_quarter_60: handleDepth,
     ok_sub_futureusd_btc_trade_quarter: handleTrade
-}, jsDao.get('api-key'), jsDao.get('secret-key'));
-
-api.start()
+})
+.setPrivateKeys(jsDao.get('api-key'), jsDao.get('secret-key'))
+.isFutures()
+.start()
