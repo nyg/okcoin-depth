be966846fe4686b72af95429aa3cdf8bbba6f855
nyg <nyg@users.noreply.github.com>
2019-01-11 21:22:07 +0100

    Fuck okcoin and their non-existent api.

--- js/controller.js
+++ js/controller.js
@@ -1,11 +1,8 @@
 view.restoreApiKeys() // from localStorage
 
-new OKCoin()
-.setChannels({
-    ok_sub_futureusd_btc_index: handleIndex,
-    ok_sub_future_btc_depth_quarter_60: handleDepth,
-    ok_sub_futureusd_btc_trade_quarter: handleTrade
-})
+new OKCoin('wss://real.okex.com:10441/websocket')
+.addChannelHandler('ok_sub_futureusd_btc_index', handleIndex)
+.addChannelHandler('ok_sub_future_btc_depth_quarter_60', handleDepth)
+.addChannelHandler('ok_sub_futureusd_btc_trade_quarter', handleTrade)
 .setPrivateKeys(jsDao.get('api-key'), jsDao.get('secret-key'))
-.isFutures()
 .start()
